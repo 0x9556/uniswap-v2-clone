4923b36c7c757380848f50929410284d8db386de
0x9556 <0x449556@skiff.com>
2023-08-23 16:08:18 +0800

    56

--- test/Router.t.sol
+++ test/Router.t.sol
@@ -17,20 +17,66 @@ contract RouterTest is RouterHelper {
         uint tokenBAmount = 4 ether;
         uint expectLiquidity = 2 ether - MINIMUM_LIQUIDITY;
 
-        (uint amountA, uint amountB, uint liquidity) = IRouter(router)
-            .addLiquidity(
-                address(tokenA),
-                address(tokenB),
-                tokenAAmount,
-                tokenBAmount,
-                0,
-                0,
-                address(this),
-                type(uint).max
-            );
+        (, , uint liquidity) = IRouter(router).addLiquidity(
+            address(tokenA),
+            address(tokenB),
+            tokenAAmount,
+            tokenBAmount,
+            0,
+            0,
+            address(this),
+            type(uint).max
+        );
 
         assertEq(liquidity, expectLiquidity);
-        assertEq(amountA, 1 ether);
-        assertEq(amountB, 4 ether);
+        assertEq(address(router).balance, 0);
+    }
+
+    function test_addLiquidityETH() public {
+        ERC20(tokenB).approve(address(router), type(uint).max);
+
+        uint tokenBAmount = 1 ether;
+        uint ETHAmount = 4 ether;
+        uint expectLiquidity = 2 ether - MINIMUM_LIQUIDITY;
+
+        (, , uint liquidity) = IRouter(router).addLiquidityETH{
+            value: ETHAmount
+        }(address(tokenB), tokenBAmount, 0, 0, address(this), type(uint).max);
+
+        assertEq(liquidity, expectLiquidity);
+        assertEq(address(router).balance, 0);
+    }
+
+    function test_removeLiquidity() public {
+        uint liquidity = addLiquidity(1 ether, 4 ether);
+        pair.approve(address(router), type(uint).max);
+
+        IRouter(router).removeLiquidity(
+            address(tokenA),
+            address(tokenB),
+            liquidity,
+            0,
+            0,
+            user,
+            type(uint).max
+        );
+
+        uint expectAmountA = 1 ether - 500;
+        uint expectAmountB = 4 ether - 2000;
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(tokenA.balanceOf(user), expectAmountA);
+        assertEq(tokenB.balanceOf(user), expectAmountB);
+    }
+
+    //internal
+
+    function addLiquidity(
+        uint tokenAAmount,
+        uint tokenBAmount
+    ) internal returns (uint liquidity) {
+        tokenA.transfer(address(pair), tokenAAmount);
+        tokenB.transfer(address(pair), tokenBAmount);
+        liquidity = pair.mint(address(this));
     }
 }
